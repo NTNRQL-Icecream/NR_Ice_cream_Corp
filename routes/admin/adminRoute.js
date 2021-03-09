77bbc1b39afdd69bbb04ff8ccb08627ea2738cc8
Nathan Peek <nmp14fsu@gmail.com>
2021-03-09 03:44:22 -0500

    Create request for accessing admin with login info. sends 404 if not found

--- routes/admin/adminRoute.js
+++ routes/admin/adminRoute.js
@@ -3,9 +3,22 @@ const Admin = require("../../models/Admin");
 const nanoid = require("nanoid");
 
 router.get("/", (req, res) => {
-    Admin.findAll().then((data) => {
-        res.json(data);
-    });
+    const { user, pass } = req.query;
+    Admin.findOne({
+        where: {
+            username: user,
+            password: pass
+        },
+        attributes: {
+            exclude: "password"
+        }
+    }).then((adminData) => {
+        if (adminData) {
+            res.status(200).json(adminData);
+        } else {
+            res.status(404).send("Incorrect login info")
+        }
+    })
 });
 
 router.post("/", async (req, res) => {
