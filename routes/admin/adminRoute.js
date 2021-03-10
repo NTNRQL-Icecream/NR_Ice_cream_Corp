51614b2aa69aa0e08a80482d3a44c11143facf43
Nathan Peek <nmp14fsu@gmail.com>
2021-03-09 23:39:05 -0500

    Edit login method to use Admin checkpassword method so passwords will be secure

--- routes/admin/adminRoute.js
+++ routes/admin/adminRoute.js
@@ -2,30 +2,46 @@ const router = require("express").Router();
 const Admin = require("../../models/Admin");
 const nanoid = require("nanoid");
 
-router.get("/", (req, res) => {
-    const { user, pass } = req.query;
-    Admin.findOne({
-        where: {
-            username: user,
-            password: pass
-        },
-        attributes: {
-            exclude: "password"
+router.get("/login", async (req, res) => {
+    try {
+        const { user, pass } = req.query;
+        const adminData = await Admin.findOne({
+            where: {
+                username: user,
+            },
+        });
+
+        // Check if not found
+        if (!adminData) {
+            res.status(404).json({
+                message: "Incorrect username or password."
+            });
+            return
         }
-    }).then((adminData) => {
-        if (adminData) {
-            res.status(200).json(adminData);
-        } else {
-            res.status(404).send("Incorrect login info")
+        // Check if valid password
+        const validPass = await adminData.passwordCheck(pass);
+        if (!validPass) {
+            res
+                .status(404)
+                .json({ message: "incorrect username or password." });
+            return;
         }
-    })
+        res.json({ admin: adminData, message: "Successful login" });
+    } catch (e) {
+        res.status(500).json(e);
+    }
 });
 
 router.post("/", async (req, res) => {
-    const body = req.body;
-    body.admin_id = nanoid.nanoid(12);
-    const admin = await Admin.create(body);
-    res.json(admin);
+    try {
+        const body = req.body;
+        body.admin_id = nanoid.nanoid(12);
+        const admin = await Admin.create(body);
+
+        res.status(201).json(admin);
+    } catch (e) {
+        res.status(500).json(e);
+    }
 });
 
 module.exports = router;
